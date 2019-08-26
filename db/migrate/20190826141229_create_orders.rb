21f4c85065968b7eafe9a47e054f460641a745e9
Devin Bombay <dbombay@Devins-MBP.fios-router.home>
2019-08-26 10:14:30 -0400

    initial commit

---
+++ db/migrate/20190826141229_create_orders.rb
@@ -0,0 +1,12 @@
+class CreateOrders < ActiveRecord::Migration[6.0]
+  def change
+    create_table :orders, id: :uuid do |t|
+      t.string :type
+      t.string :color
+      t.integer :quantity
+      t.datetime :need_by
+
+      t.timestamps
+    end
+  end
+end
